998c63147c19617b63396e06de5affe25deffc0a
Joseph Pal <josephpal27@gmail.com>
2025-01-28 16:48:21 +0530

    add new icons in various formats and update slider functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,41 +62,41 @@ window.addEventListener("scroll", () => {
 
 // Functionality For Slider
 
-let currentIndex = 0;
-const slides = document.querySelector(".slides");
-const totalSlides = document.querySelectorAll(".slide").length;
-const intervalTime = 3000; // 3 seconds
+// let currentIndex = 0;
+// const slides = document.querySelector(".slides");
+// const totalSlides = document.querySelectorAll(".slide").length;
+// const intervalTime = 3000; // 3 seconds
 
-function updateSlidePosition() {
-  const newTransform = -currentIndex * 100;
-  slides.style.transform = `translateX(${newTransform}%)`;
-}
+// function updateSlidePosition() {
+//   const newTransform = -currentIndex * 100;
+//   slides.style.transform = `translateX(${newTransform}%)`;
+// }
 
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % totalSlides;
-  updateSlidePosition();
-}
+// function nextSlide() {
+//   currentIndex = (currentIndex + 1) % totalSlides;
+//   updateSlidePosition();
+// }
 
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-  updateSlidePosition();
-}
+// function prevSlide() {
+//   currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+//   updateSlidePosition();
+// }
 
-document.querySelector(".next").addEventListener("click", nextSlide);
-document.querySelector(".prev").addEventListener("click", prevSlide);
+// document.querySelector(".next").addEventListener("click", nextSlide);
+// document.querySelector(".prev").addEventListener("click", prevSlide);
 
-// Auto-slide functionality
-let slideInterval = setInterval(nextSlide, intervalTime);
+// // Auto-slide functionality
+// let slideInterval = setInterval(nextSlide, intervalTime);
 
-// Pause the auto-slide when hovering over the carousel
-document.querySelector(".carousel").addEventListener("mouseenter", () => {
-  clearInterval(slideInterval);
-});
+// // Pause the auto-slide when hovering over the carousel
+// document.querySelector(".carousel").addEventListener("mouseenter", () => {
+//   clearInterval(slideInterval);
+// });
 
-// Resume auto-slide when not hovering
-document.querySelector(".carousel").addEventListener("mouseleave", () => {
-  slideInterval = setInterval(nextSlide, intervalTime);
-});
+// // Resume auto-slide when not hovering
+// document.querySelector(".carousel").addEventListener("mouseleave", () => {
+//   slideInterval = setInterval(nextSlide, intervalTime);
+// });
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
